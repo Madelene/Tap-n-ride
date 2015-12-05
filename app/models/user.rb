a4e2deef6c92eebf47158a795d68f0eefe6bf39e
Madelene Campos <madelenecampos@gmail.com>
2015-12-04 22:57:17 -0500

    Added to_s method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: {
     with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
     }
+
+  def to_s
+  "#{first_name} #{last_name}"
+  end
 end
