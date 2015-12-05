21acd08e896a2a7325fa1fa11f96db489043eab8
tjr05d <tjr05d@my.fsu.edu>
2015-12-05 00:24:52 -0500

    Created method in the application controller to allow sessions controller to have access to the user information

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: {
-    with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+    with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
     }
 
   def to_s
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205033439) do
+ActiveRecord::Schema.define(version: 20151205041130) do
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "trip_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "stations", ["trip_id"], name: "index_stations_on_trip_id"
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "enter_station"
+    t.string   "exit_station"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
