962333419151ff1ea813403cfbde7081e7050a48
Madelene Campos <madelenecampos@gmail.com>
2015-12-07 17:53:34 -0500

    Added credits column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205041130) do
+ActiveRecord::Schema.define(version: 20151207224759) do
 
   create_table "stations", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20151205041130) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "credit"
   end
 
 end
